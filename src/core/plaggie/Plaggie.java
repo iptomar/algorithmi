6254e4aa881950f585d94eaf6cca5f420283a519
devlmarques <luis-marques.alumni@sapo.pt>
2016-04-21 22:13:48 +0100

    Class modified

--- src/core/plaggie/Plaggie.java
+++ src/core/plaggie/Plaggie.java
@@ -27,34 +27,33 @@ import plag.parser.plaggie.Configuration;
  * @author Luis
  */
 public class Plaggie {
-      public static double[][] compare2( String[]label, String[] code) throws FileNotFoundException, IOException, NullPointerException {
+    
+    public static double[][] compare2( String[]label, String[] code) throws FileNotFoundException, IOException, NullPointerException {
+        
         try{ 
-            
             int dim = label.length;
-            
-            //desenha primeira linha
             for (int i = 0; i < dim; i++) {
                 if(i==0)
-                    System.out.print("\n\t| ");
+                    System.out.print("\n     |  ");
                 else
-                    System.out.print(" | ");
+                    System.out.print("   |   ");
                 System.out.print(label[i]);
             }
-            System.out.print(" |");
+            System.out.print("  |");
             for (int i = 0; i < dim; i++) {
                 for (int j = 0; j < dim; j++) {
                     if(j==0)
-                        System.out.print("\n|"+label[i]+"\t| ");
+                        System.out.print("\n| "+label[i]+" | ");
                     else
                         System.out.print(" ");
-                    System.out.print(compare2( code[i], code[j])+" |");
+                    System.out.print(" "+Stats.getPercentage(compare2( code[i], code[j]))+" |");
                 }
             }        
             System.out.println("\n");
         }catch (Exception ex) {
             Logger.getLogger(core.plaggie.Plaggie.class.getName()).log(Level.SEVERE, null, ex);
-        } 
-                     
+        }
+        
         return null;
         
     }
@@ -67,25 +66,27 @@ public class Plaggie {
             
             TokenSimilarityChecker tokenChecker = new SimpleTokenSimilarityChecker(3);
             
-            // -- Create the code tokenizer object for parsing the source code files
+            // -- Criar o objecto de código tokenizer para parsing dos ficheiros de código fonte
             Configuration config = new PlaggieConfiguration();
                        
 	    CodeTokenizer codeTokenizer = (CodeTokenizer)Class.forName(config.codeTokenizer).newInstance();
             SubmissionSimilarityChecker check = new SimpleSubmissionSimilarityChecker(tokenChecker, codeTokenizer);
             
             SubmissionDetectionResult detResult = 
-		    new SubmissionDetectionResult(subA, subB, check, 0.5); 
-            //return (int)Math.round(detResult.getMaxFileSimilarityProduct());
-            //return Math.round(detResult.getMaxFileSimilarityProduct());
-            return detResult.getMaxFileSimilarityProduct();
+                new SubmissionDetectionResult(subA, subB, check, 0.5); 
             
+            return detResult.getMaxFileSimilarityProduct();
+
         } catch (Exception ex) {
             Logger.getLogger(core.plaggie.Plaggie.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
         return 0;
+        
     }
     
     public static void main(String[] args) throws FileNotFoundException, IOException, NullPointerException {
+        
         try {
             Stats.newCounter("submissions");
             Stats.newCounter("parsed_files");
@@ -96,35 +97,27 @@ public class Plaggie {
             Stats.newCounter("similarity_over_threshold");
             Stats.newCounter("similarity_comparisons");
             Stats.newCounter("blacklisted_detection_results");
-            
             Stats.newDistribution("files_in_submission");
             Stats.newDistribution("submission_similarities");
             Stats.newDistribution("submission_similarities_a");
             Stats.newDistribution("submission_similarities_b");
             Stats.newDistribution("maximum_file_similarities");
-            
         } catch (StatsException ex) {
             Logger.getLogger(Plaggie.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-//        String alunos[]={"Ana","Beta","Carla","Dora","Filipa","Joana","Maria"};
-//        String ficheiros[]={
-//            "D:/Submissions/Ana/Codigo.java",
-//            "D:/Submissions/Beta/Codigo.java",
-//            "D:/Submissions/Carla/Codigo.java",
-//            "D:/Submissions/Dora/Codigo.java",
-//            "D:/Submissions/Filipa/Codigo.java",  
-//            "D:/Submissions/Joana/Codigo.java",
-//            "D:/Submissions/Maria/Codigo.java"};  
-        String alunos[]={"N0","N1","N2","N3","N4","N5","N6"};
+        String alunos[]={"N0","N1","N2","N3","N4","N5","N6","N7","N8"};
+
         String ficheiros[]={
-            "D:/Submissions/N0/Codigo.java",
-            "D:/Submissions/N1/Codigo.java",
-            "D:/Submissions/N2/Codigo.java",
-            "D:/Submissions/N3/Codigo.java",
-            "D:/Submissions/N4/Codigo.java",  
-            "D:/Submissions/N5/Codigo.java",
-            "D:/Submissions/N6/Codigo.java"}; 
+            "/myGitHub/LEI2016PSI/Algorithmi-Core-Plaggie-Git/algorithmi-code/src/core/plaggie/plaggieTesteDrive/Submissions/Nivel0/Factorial.java.teste",
+            "/myGitHub/LEI2016PSI/Algorithmi-Core-Plaggie-Git/algorithmi-code/src/core/plaggie/plaggieTesteDrive/Submissions/Nivel1/Factorial.java.teste",
+            "/myGitHub/LEI2016PSI/Algorithmi-Core-Plaggie-Git/algorithmi-code/src/core/plaggie/plaggieTesteDrive/Submissions/Nivel2/Factorial.java.teste",
+            "/myGitHub/LEI2016PSI/Algorithmi-Core-Plaggie-Git/algorithmi-code/src/core/plaggie/plaggieTesteDrive/Submissions/Nivel3/Factorial.java.teste",
+            "/myGitHub/LEI2016PSI/Algorithmi-Core-Plaggie-Git/algorithmi-code/src/core/plaggie/plaggieTesteDrive/Submissions/Nivel4/Factorial.java.teste",
+            "/myGitHub/LEI2016PSI/Algorithmi-Core-Plaggie-Git/algorithmi-code/src/core/plaggie/plaggieTesteDrive/Submissions/Nivel5/Factorial.java.teste",
+            "/myGitHub/LEI2016PSI/Algorithmi-Core-Plaggie-Git/algorithmi-code/src/core/plaggie/plaggieTesteDrive/Submissions/Nivel6/Factorial.java.teste",
+            "/myGitHub/LEI2016PSI/Algorithmi-Core-Plaggie-Git/algorithmi-code/src/core/plaggie/plaggieTesteDrive/Submissions/Nivel7/Factorial.java.teste",
+            "/myGitHub/LEI2016PSI/Algorithmi-Core-Plaggie-Git/algorithmi-code/src/core/plaggie/plaggieTesteDrive/Submissions/Nivel8/Factorial.java.teste"};         
         try{
             compare2( alunos, ficheiros);
         }catch (IOException | NullPointerException ex) {
